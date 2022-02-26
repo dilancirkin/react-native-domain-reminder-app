28c0bc77b3a5574d601448db5ca1a8d13e48fe1f
dilancirkin <dilancirkin@gmail.com>
2022-02-26 20:36:07 +0300

    packages installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, {useEffect} from 'react';
 import Navigation from './Navigation/Navigation';
+import {LogBox} from 'react-native';
 
+LogBox.ignoreLogs([
+  "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
+]);
 export default function App() {
-
-  return (
-
-      <Navigation />
-  );
+  return <Navigation />;
 }
